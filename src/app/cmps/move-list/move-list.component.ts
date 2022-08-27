dd1745c0adcd3786d5d0a1952de7710c31dfc0fd
BarBronshtein <barbronshtein@gmail.com>
2022-08-27 18:25:49 +0300

    added functionallity to move-list cmp

--- src/app/cmps/move-list/move-list.component.ts
+++ src/app/cmps/move-list/move-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Contact } from 'src/app/models';
+import { Move } from 'src/app/models/move.model';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'move-list',
   templateUrl: './move-list.component.html',
-  styleUrls: ['./move-list.component.scss']
+  styleUrls: ['./move-list.component.scss'],
 })
 export class MoveListComponent implements OnInit {
+  constructor(private userService: UserService) {}
 
-  constructor() { }
-
+  @Input() contact!: Contact | undefined;
+  user!: User;
+  moves!: Move[];
   ngOnInit(): void {
+    this.user = this.userService.loggedinUser;
+    if (this.contact)
+      this.moves = this.userService.getMoves(this.contact._id as string);
+    else this.moves = this.userService.getLastMoves();
   }
-
 }
--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { UserMsgService } from './user-msg.service';
 import { UserService } from './user.service';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private userMsgService: UserMsgService
+  ) {}
   canActivate(): boolean {
     if (!this.userService.loggedinUser) {
       this.router.navigateByUrl('/signup');
+      this.userMsgService.setUserMsg(
+        'Not logged in please signup if you dont have an acoount'
+      );
       return false;
     }
     return true;
