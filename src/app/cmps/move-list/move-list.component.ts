ab9995fdba687e6aa4116636c728657df700b975
BarBronshtein <barbronshtein@gmail.com>
2022-08-29 10:24:13 +0300

    fixed getMoves function

--- src/app/cmps/move-list/move-list.component.ts
+++ src/app/cmps/move-list/move-list.component.ts
@@ -23,7 +23,7 @@ export class MoveListComponent implements OnInit {
 
   getMoves() {
     if (this.contact?._id) this.moves = this.user.moves;
-    else this.moves = this.user.moves.slice(this.user.moves.length - 3);
+    else this.moves = this.user.moves.slice(-3).sort(() => -1);
   }
 
   // getMoves() {
